ccbdd90705efa634e883b4886b0939ec4cde879d
Vlado <vla.vladinov.tues19@gmail.com>
2023-10-09 12:41:51 +0300

    Implement AI attack range

---
+++ Source/ActionRoguelike/Private/AI/SBTService_CheckAttackRange.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/SBTService_CheckAttackRange.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h"
+
+void USBTService_CheckAttackRange::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
+
+	if (ensure(BlackboardComp))
+	{
+		AActor* TargetActor = Cast<AActor>(BlackboardComp->GetValueAsObject("TargetActor"));
+
+		if (TargetActor)
+		{
+			AAIController* AIController = OwnerComp.GetAIOwner();
+			if (ensure(AIController))
+			{
+				APawn* AIPawn = AIController->GetPawn();
+				if (ensure(AIPawn))
+				{
+					float DistanceTo = FVector::Distance(TargetActor->GetActorLocation(), AIPawn->GetActorLocation());
+
+					bool bWithinDistance = DistanceTo < 2000.0f;
+					bool bHasLOS = false;
+					
+					if (bWithinDistance)
+					{
+						bHasLOS = AIController->LineOfSightTo(TargetActor);
+					}
+
+
+					BlackboardComp->SetValueAsBool(AttackRangeKey.SelectedKeyName, (bWithinDistance && bHasLOS));
+				}
+			}
+		}
+	}
+}
